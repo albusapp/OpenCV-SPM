b65fecfd3ae689f59b64b112ea3635a86b29dbf9
Nathan SALAUN <nathan.salaun@sofia.dev>
2023-07-10 16:56:23 +0200

    First commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.8
+
+import PackageDescription
+
+let package = Package(
+    name: "OpenCV-SPM",
+    products: [
+        .library(
+            name: "OpenCV",
+            targets: ["OpenCV"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "OpenCV",
+            url: "https://github.com/albusapp/opencv/releases/download/3.4.16-universal/opencv-3.4.16-ios-xcframework.zip",
+            checksum: "fd1d97b5c3e52cb4f84bb4718113db80ec1726eb621c5477593182e19dba4d6b"
+        )
+    ]
+)
