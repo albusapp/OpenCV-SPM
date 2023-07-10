3f0bcb93fe503f107ae5744b97441b5befb5b3f4
Nathan SALAUN <nathan.salaun@sofia.dev>
2023-07-10 17:29:29 +0200

    Rename target and product to opencv2

--- Package.swift
+++ Package.swift
@@ -6,13 +6,13 @@ let package = Package(
     name: "OpenCV-SPM",
     products: [
         .library(
-            name: "OpenCV",
-            targets: ["OpenCV"]
+            name: "opencv2",
+            targets: ["opencv2"]
         )
     ],
     targets: [
         .binaryTarget(
-            name: "OpenCV",
+            name: "opencv2",
             url: "https://github.com/albusapp/opencv/releases/download/3.4.16-universal/opencv-3.4.16-ios-xcframework.zip",
             checksum: "fd1d97b5c3e52cb4f84bb4718113db80ec1726eb621c5477593182e19dba4d6b"
         )
